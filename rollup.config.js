fc108f33176a609c3371b823b53a24db17cdc91a
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-02-12 22:39:33 +0100

    refactoring sass -> scss / config rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,65 +2,84 @@
 /* eslint-env es6 */
 
 const cleanup = require('rollup-plugin-cleanup');
-// const dts = require('rollup-plugin-dts').default;
 const json = require('@rollup/plugin-json');
 const resolve = require('@rollup/plugin-node-resolve').default;
 const terser = require('rollup-plugin-terser').terser;
 const pkg = require('./package.json');
+import scss from 'rollup-plugin-scss';
 
-const input = 'src/js/application.js';
-const inputESM = {
-    'dist/chart.esm': 'src/index.esm.js',
-};
-// const inputESMTypings = {
-//     'dist/chart.esm': 'types/index.esm.d.ts',
-//     'dist/helpers.esm': 'types/helpers/index.d.ts'
-// };
+const inputJS = 'src/js/application.js';
+const inputSCSS = 'src/scss/main.scss';
 
 const banner = `/*!
  * lotivis.js v${pkg.version}
  * ${pkg.homepage}
- * (c) ${(new Date(process.env.SOURCE_DATE_EPOCH ? (process.env.SOURCE_DATE_EPOCH * 1000) : new Date().getTime())).getFullYear()} lotivis.js Contributors
+ * (c) ${(new Date(process.env.SOURCE_DATE_EPOCH ? (process.env.SOURCE_DATE_EPOCH * 1000) : new Date().getTime())).getFullYear()} lotivis.js Lukas Danckwerth
  * Released under the MIT License
  */`;
 
 module.exports = [
-    // UMD builds
-    // dist/chart.min.js
-    // dist/chart.js
-    {
-        input,
-        plugins: [
-            json(),
-            resolve(),
-            cleanup({
-                sourcemap: true
-            })
-        ],
-        output: {
-            name: 'lotivis',
-            file: 'dist/lotivis.js',
-            banner,
-            format: 'umd',
-            indent: false,
-        },
+  // UMD builds
+  // dist/lotivis.min.js
+  // dist/lotivis.js
+  {
+    input: inputJS,
+    plugins: [
+      json(),
+      resolve(),
+      cleanup({
+        sourcemap: true
+      })
+    ],
+    output: {
+      name: 'lotivis',
+      file: 'dist/lotivis.js',
+      banner,
+      format: 'umd',
+      indent: false,
     },
-    {
-        input,
-        plugins: [
-            json(),
-            resolve(),
-            terser({
-                output: {
-                    preamble: banner
-                }
-            })
-        ],
+  },
+  {
+    input: inputJS,
+    plugins: [
+      json(),
+      resolve(),
+      terser({
         output: {
-            name: 'lotivis',
-            file: 'dist/lotivis.min.js',
-            format: 'umd',
-            indent: false,
-        },
+          preamble: banner
+        }
+      })
+    ],
+    output: {
+      name: 'lotivis',
+      file: 'dist/lotivis.min.js',
+      format: 'umd',
+      indent: false,
     },
+  },
+  {
+    input: inputSCSS,
+    plugins: [
+      scss({
+        output: "./dist/lotivis.css",
+        failOnError: true
+      })
+    ],
+    output: {
+      file: "./dist/lotivis.min.css"
+    }
+  },
+  {
+    input: inputSCSS,
+    plugins: [
+      scss({
+        output: "./dist/lotivis.min.css",
+        outputStyle: "compressed",
+        failOnError: true
+      })
+    ],
+    output: {
+      file: "./dist/lotivis.min.css"
+    }
+  }
 ];
