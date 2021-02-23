ff82268d53b4d63b2087e906c9d3051505dbec4c
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-02-23 13:46:17 +0100

    add example

---
+++ server.js
@@ -0,0 +1,20 @@
+/**
+ * FRC-Visualization
+ *
+ * Author:
+ *  - Lukas Danckwerth
+ */
+const express = require("express");
+const app = express();
+const application_version = require('./package.json').version;
+const port = process.env.PORT || 80;
+const environment = process.env.NODE_ENV || 'development';
+
+app.use('/', express.static(__dirname + '/example'));
+app.use('/dist', express.static(__dirname + '/dist'));
+
+app.listen(port, function() {
+    console.log("start FRC-Visualization " + application_version);
+    console.log("listening on port: " + port);
+    console.log('environment: ' + environment);
+});
