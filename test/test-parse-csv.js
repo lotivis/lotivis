f58601899104869b9e68149c0ea5d5a6a6532fae
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-03-05 00:26:08 +0100

    update tests

--- test/test-parse-csv.js
+++ test/test-parse-csv.js
@@ -4,19 +4,19 @@ const lotivis = require('../dist/lotivis');
 
 describe('parse csv', function () {
 
-  it('some', async function () {
-    let url = 'https://phpefi.schleswig-holstein.de/corona/data202011/cvd_sh_verlauf.csv';
-    let flatData = await lotivis.parseCSV(url, function (components) {
-      return {
-        date: String(components[0]),
-        value: String(components[1])
-      };
-    })
-      .then(function (dataset) {
-
-      });
-
-    assert.strictEqual('a', 'a');
-  });
+  // it('some', async function () {
+  //   let url = 'https://phpefi.schleswig-holstein.de/corona/data202011/cvd_sh_verlauf.csv';
+  //   let flatData = await lotivis.parseCSV(url, function (components) {
+  //     return {
+  //       date: String(components[0]),
+  //       value: String(components[1])
+  //     };
+  //   })
+  //     .then(function (dataset) {
+  //
+  //     });
+  //
+  //   assert.strictEqual('a', 'a');
+  // });
 
 });
