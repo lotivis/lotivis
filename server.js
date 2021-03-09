24bff097b212f171df8d2a75c7611c5668d69d00
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-03-09 10:40:23 +0100

    refactor css

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 /**
- * FRC-Visualization
+ * server.js lotivis
+ *
+ * Discussion:
+ * Serves the examples of lotivis.  Using an Node.js express app for as server.
  *
  * Author:
  *  - Lukas Danckwerth
@@ -10,15 +13,13 @@ const version = require('./package.json').version;
 const port = process.env.PORT || 80;
 
 app.use('/', express.static(__dirname + '/examples'));
-
 app.use('/dist', express.static(__dirname + '/dist'));
-
-app.use('/version', function (req,res) {
+app.use('/version', function (req, res) {
   res.json({version});
   res.end();
 });
 
-app.listen(port, function() {
-    console.log("start lotivis examples " + version);
-    console.log("listening on port: " + port);
+app.listen(port, function () {
+  console.log("start lotivis examples " + version);
+  console.log("listening on port: " + port);
 });
