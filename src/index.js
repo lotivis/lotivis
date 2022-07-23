bc0a0d2dfc6ff5263e34f81b3c612c396591d71e
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2022-07-23 16:43:45 +0200

    remove tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-export * from "lotivis-csv";
 export * from "lotivis-chart";
 export * from "lotivis-colors";
-export * from "lotivis-charts";
+export * from "lotivis-csv";
+// export * from "lotivis-charts";
 export * from "lotivis-data";
 export * from "lotivis-datasets";
 export * from "lotivis-export";
